1ee70597348a04bb3e9096a3824567464a6741d0
unknown <gabrielhenryque311@gmail.com>
2023-02-18 19:53:36 -0300

    adicionado_projeto

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ df = pd.DataFrame(columns=["nome","descricao","quantidade","valor"])
 ## Dicionário a ser adicionado ao DataFrame
 while True:
     novo_registro = {"nome":"","descricao":"","quantidade":"","valor":""}
-    novo_registro["nome"] = input("Qual é o nome do produto? ")
+    ##
+    novo_registro["nome"] = input("Qual é o nome do produto? ") 
+    ##
     novo_registro["descricao"] = input("Por favor, insira a descrição do produto: ")
+    ##
     novo_registro["quantidade"] = input("Por favor, insira a quantidade do produto: ")
+    ##
     novo_registro["valor"] = input("Por favor, insira o valor do produto: ")
     
     ok = input("Deseja arquivar este produto? ")
